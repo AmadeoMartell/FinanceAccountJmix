a71669f06472d7686b757cc6750c2e86944d4a2b
AmadeoMartell <medeu207@gmail.com>
2025-06-25 05:24:02 +0500

    Implemented service usage with TransactionType page

--- src/main/java/com/company/financeaccountjmix/view/transactiontype/TransactionTypeDetailView.java
+++ src/main/java/com/company/financeaccountjmix/view/transactiontype/TransactionTypeDetailView.java
@@ -1,16 +1,50 @@
 package com.company.financeaccountjmix.view.transactiontype;
 
+import com.company.financeaccountjmix.app.service.TransactionTypeService;
+import com.company.financeaccountjmix.entity.Client;
 import com.company.financeaccountjmix.entity.TransactionType;
+import com.company.financeaccountjmix.entity.User;
 import com.company.financeaccountjmix.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.flowui.component.valuepicker.EntityPicker;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+
+import java.util.UUID;
 
 @Route(value = "transaction-types/:id", layout = MainView.class)
 @ViewController(id = "TransactionType.detail")
 @ViewDescriptor(path = "transaction-type-detail-view.xml")
 @EditedEntityContainer("transactionTypeDc")
 public class TransactionTypeDetailView extends StandardDetailView<TransactionType> {
+
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @ViewComponent("clientField")
+    private EntityPicker<Client> clientField;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<TransactionType> event) {
+        User user = (User) currentAuthentication.getAuthentication().getPrincipal();
+
+        event.getEntity().setClient((Client) user);
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        clientField.setEnabled(false);
+
+        TransactionType tt = getEditedEntity();
+        if (tt.getId() != null) {
+            UUID currentUserId = ((User) currentAuthentication
+                    .getAuthentication().getPrincipal()).getId();
+            if (!tt.getClient().getId().equals(currentUserId)) {
+                throw new AccessDeniedException("Нет доступа к этому типу транзакции");
+            }
+        }
+    }
 }
--- src/main/java/com/company/financeaccountjmix/view/transactiontype/TransactionTypeListView.java
+++ src/main/java/com/company/financeaccountjmix/view/transactiontype/TransactionTypeListView.java
@@ -1,16 +1,20 @@
 package com.company.financeaccountjmix.view.transactiontype;
 
+import com.company.financeaccountjmix.app.service.TransactionTypeService;
 import com.company.financeaccountjmix.entity.Client;
 import com.company.financeaccountjmix.entity.TransactionType;
 import com.company.financeaccountjmix.entity.User;
 import com.company.financeaccountjmix.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
+
+import java.util.List;
 
 
 @Route(value = "transaction-types", layout = MainView.class)
@@ -20,16 +24,18 @@ import org.springframework.security.core.Authentication;
 @DialogMode(width = "64em")
 public class TransactionTypeListView extends StandardListView<TransactionType> {
 
-    @ViewComponent
-    private CollectionLoader<TransactionType> transactionTypesDl;
+    @ViewComponent("transactionTypesDc")
+    private CollectionContainer<TransactionType> transactionTypesDc;
+
+    @Autowired
+    private TransactionTypeService transactionTypeService;
+
     @Autowired
     private CurrentAuthentication currentAuthentication;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
-        User user = (User) currentAuthentication.getAuthentication().getPrincipal();
-        transactionTypesDl.setParameter("userId", user.getId());
-        transactionTypesDl.load();
+        List<TransactionType> list = transactionTypeService.getMyTypes();
+        transactionTypesDc.setItems(list);
     }
-
 }
